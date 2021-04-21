c7f13783cc6be2f91aaceee4c179cd3c657d503d
fyqc <67112028+fyqc@users.noreply.github.com>
2021-04-21 12:38:01 -0400

    《GTA5》线上赌场转盘自动操作，抽取头奖

---
+++ Mijikai Essei/casinoinput.py
@@ -0,0 +1,14 @@
+# 《GTA5》线上赌场转盘自动操作，抽取头奖
+
+import time
+import pyautogui
+
+
+time.sleep(10)
+
+pyautogui.typewrite('hello!')
+
+
+pyautogui.typewrite(['e'])
+time.sleep(4)
+pyautogui.typewrite(['s'])
