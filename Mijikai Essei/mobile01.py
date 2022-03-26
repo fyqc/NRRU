f1654c9c0eb31029d7a6ab2e95adbf9eebd2268c
fyqc <67112028+fyqc@users.noreply.github.com>
2022-03-26 08:20:14 -0400

    爬取Mobile01上照片原图的程式第二版
    增加了对2019年及之前的页面的支持

--- Mijikai Essei/mobile01.py
+++ Mijikai Essei/mobile01.py
@@ -1,19 +1,17 @@
 import os
 import requests
-import time
 from bs4 import BeautifulSoup
 from threading import Thread
 
-
-# 初代爬取Mobile01上照片原图的程式
-# 2022年3月19日
+# 爬取Mobile01上照片原图的程式第二版
+# 2022年3月22日
 
 # Fujifilm GFX50S II 評測報告｜6.5級 IBIS 超有感 規格齊備的中片幅之選！ - Mobile01
 
 HEADERS = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 12_0_1) AppleWebKit/605.1.15 "
            "(KHTML, like Gecko) Version/15.0 Safari/605.1.15"}
-SAVE_DIRECTORY = r'D:\Imperial Readings\My pictures\Mobile01\2020\2020 Mobile01'
-SAVE_DIRECTORY_H = r'D:\Imperial Readings\My pictures\Mobile01\2020\2020 Mobile01'
+SAVE_DIRECTORY = r'F:\Pictures\Mobile01\2018\322'
+SAVE_DIRECTORY_H = r'F:\Pictures\Mobile01\2018\322'
 
 
 def withdraw_data_src(content):
@@ -62,22 +60,31 @@ def extract_image_url(soup):
     h_downlist = []
     l_downlist = []
     r_downlist = []
+
     div_itemprop_articleBody = soup.find('div', itemprop="articleBody")
 
     # 高清部分
     tag_a = div_itemprop_articleBody.find_all('a')
+
+
     for content in tag_a:
 
         img_url_high_resolution = content.get('href')
-        if img_url_high_resolution and 'https://attach.mobile01.com/attach/' in img_url_high_resolution:
-            h_downlist.append(img_url_high_resolution)
+        if img_url_high_resolution:
+            if 'https://attach.mobile01.com/attach/' in img_url_high_resolution:
+                h_downlist.append(img_url_high_resolution)
+            elif '//attach.mobile01.com/attach/' in img_url_high_resolution:
+                h_downlist.append("".join(['https:', img_url_high_resolution]))
 
         # 找到重复的非高清的图片
         for item in content:
             try:
                 img_url_repeat = item.get('data-src')
-                if img_url_repeat and 'https://attach.mobile01.com/attach/' in img_url_repeat:
-                    r_downlist.append(img_url_repeat)
+                if img_url_repeat:
+                    if 'https://attach.mobile01.com/attach/' in img_url_repeat:
+                        r_downlist.append(img_url_repeat)
+                    elif '//attach.mobile01.com/attach/' in img_url_repeat:
+                        r_downlist.append("".join(['https:', img_url_repeat]))
             except:
                 pass
 
@@ -85,8 +92,11 @@ def extract_image_url(soup):
     tag_img = div_itemprop_articleBody.find_all('img')
     for content in tag_img:
         img_url_low_resolution = content.get('data-src')
-        if img_url_low_resolution and 'https://attach.mobile01.com/attach/' in img_url_low_resolution:
-            l_downlist.append(img_url_low_resolution)
+        if img_url_low_resolution:
+            if 'https://attach.mobile01.com/attach/' in img_url_low_resolution:
+                l_downlist.append(img_url_low_resolution)
+            elif '//attach.mobile01.com/attach/' in img_url_low_resolution:
+                l_downlist.append("".join(['https:', img_url_low_resolution]))
 
     # 剔除重复的非高清图片
     if len(r_downlist) < len(l_downlist):
@@ -112,6 +122,8 @@ def play():
     soup = get_soup_from_localhtml("soup.html")
     title = soup.title.string
 
+    print(title)
+
     h_list, l_list = extract_image_url(soup)
 
     if not os.path.exists(SAVE_DIRECTORY):
@@ -123,7 +135,7 @@ def play():
     bring_mm_home(h_list, SAVE_DIRECTORY_H)
     bring_mm_home(l_list, SAVE_DIRECTORY)
 
-    print(title)
+    print("\n", title, "\n")
 
 if __name__ == '__main__':
 
