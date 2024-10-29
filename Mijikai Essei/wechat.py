f4ec6799942a048e1ea699602bf1b658179b1963
fyqc <67112028+fyqc@users.noreply.github.com>
2024-10-29 01:55:26 -0400

    修复了一些Bug

--- Mijikai Essei/wechat.py
+++ Mijikai Essei/wechat.py
@@ -1,4 +1,5 @@
 import hashlib
+import logging
 import os
 import shutil
 import subprocess
@@ -8,30 +9,45 @@ import time
 from bs4 import BeautifulSoup
 from PIL import Image
 from threading import Thread
-from urllib.parse import quote, unquote
 
+"""
+用于微信公众号中的诸多图片保存
 
-# 本代码用于下载桌面浏览器能够打开的腾讯微信公众号网页中所包含的图像
-# 2024年7月25日
+以文章标题为文件夹的名字
+以图片本身的MD5作为文件名
 
+将WebP自动转换为PNG
+
+操作说明：
+把URL复制进来，放在列表中
+然后执行，就可以在目标文件夹中生成以标题为文件夹名的文件夹，里面是帖子中的图片
+"""
+
+# 2024年10月29日
+
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    # level=logging.DEBUG,
+    level=logging.INFO,
+    handlers=[logging.FileHandler(filename='tencent.log',mode='w',encoding='utf-8')]
+)
 
 HEADER = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36'
     ' (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.70',
 }
 
-STATIC = r"D:\Lab\dwebp.exe"
+STATIC = r"D:\Lab\dwebp.exe"  # 用于转换Webp格式为PNG
 SAVE_DIRECTORY = r"D:\wc"
 
-
-waiting = """
-
-
-"""
-
-
+# 针对性修改此处
 URLS = [
 
+"https://mp.weixin.qq.com/s/C4BTn_YOcn8A7ANQq6Xw2A",
+
+"https://mp.weixin.qq.com/s/ZK2-BaoiDluQphSJRvKWmw",
+
+"https://mp.weixin.qq.com/s/wrg_qVA7LLNdO26cq8pP8g",
 
 ]
 
@@ -48,6 +64,7 @@ def get_soup_from_webpage(url: str, header: str, timeout=None):
                 success = True
             else:
                 print(f"{url} 下载失败 状态码： {response.status_code}")
+                logging.error(f"{url} 下载失败 状态码： {response.status_code}")
                 break
         except:
             print(f"{url} \t 连接错误，无法加载\n")
@@ -83,22 +100,28 @@ def make_name_valid(validname: str) -> str:
     return validname
 
 
-def extract_image_url(soup: BeautifulSoup) -> list:
-    raw_down_list = []
-    try:
-        tags = soup.find_all('img')
-        for n in tags:
-            raw_down_list.append(n['data-src'])
-    except:
-        # 从 2023-03-03 开始，微信使坏，开始用JavaScript来对图片地址进行隐藏
-        hidden_section = soup.find('div', id="js_content")
-        hidden_img_tags = hidden_section.find_all('img')
-        for n in hidden_img_tags:
-            raw_down_list.append(n['data-src'])
+def extract_image_using_re_only(content: str) -> list:
+    # cdn_url: 'https://mmbiz.qpic.cn/mmbiz_jpg/AicOhKNCfiaD9SHrapreD260eWviaANiaB100VoiasAeZicAo7Hl0qVTwKfzAUWB5Su4zhuQrtWK4OCwcgCnibUUbaL1g/0?wx_fmt=jpeg',
+    # 'cdn_url':'https://mmbiz.qpic.cn/sz_mmbiz_gif/r7UskC2NBNCgceEu9iazYtvRMKezh8uAgScic6l4aM2BWwwcibmh50ofnI5uiaGAKRwDvXvWZYAS9VZdLttNhGDIxQ/640?wx_fmt=gif\x26amp;amp;from=appmsg'
+    
+    pattern = r"(?:['\"]?cdn_url['\"]?)\s*:\s*['\"]https?://mmbiz[^\s\"']*['\"]"
+    
+    cdn_urls = re.findall(pattern, content)
 
-    # 去重
-    downlist = list(set(raw_down_list))
-    return downlist
+    logging.debug(cdn_urls)
+    
+    if cdn_urls:
+        # https://mmbiz.qpic.cn/mmbiz_jpg/AicOhKNCfiaD9SHrapreD260eWviaANiaB100VoiasAeZicAo7Hl0qVTwKfzAUWB5Su4zhuQrtWK4OCwcgCnibUUbaL1g/0?wx_fmt=jpeg
+        cleaned_urls = [url.replace(" ","").split(":'")[-1].split("\\")[0] for url in cdn_urls]
+
+        # 去重
+        downlist = list(set(cleaned_urls))
+
+        return downlist
+
+    else:
+        print("No image URLs found.")
+        return None
     
 
 def extract_image_from_cdn_version_page(content: str) -> list:
@@ -150,6 +173,7 @@ def find_title(url: str, soup: BeautifulSoup) -> str:
             return 'Weixin Official Accounts Platform'
         else:
             '革命尚未成功 同志仍需努力 （未能识别标题，请修改代码）'
+            logging.error(url, '未能识别标题')
     else:
         return '革命尚未成功 同志仍需努力 （未能识别标题，请修改代码）'
 
@@ -192,13 +216,17 @@ def rillaget(url: str, folder_path: str, header: str) -> None:
         else:
             print(
                 f"Failed to download {url}. HTTP Status Code: {response.status_code}")
+            logging.error(
+                f"Failed to download {url}. HTTP Status Code: {response.status_code}")
 
     except OSError as f:
         # "Cannot create a file when that file already exists"
         print(f)
+        logging.error(f)
 
     except Exception as e:
         print(f"Error occurred while downloading {url}:\n{str(e)}")
+        logging.error(f"Error occurred while downloading {url}:\n{str(e)}")
 
 
 def rename_to_md5(filepath):
@@ -220,42 +248,86 @@ def get_md5(filepath):
     return md5_returned
 
 
-def webp_to_png(folder_path):
+def rename_to_md5(filepath):
+    file_name = filepath.split("\\")[-1].split(".")[0]
+    new_name = get_md5(filepath)
+    new_path = filepath.replace(file_name, new_name)
+    try:
+        os.rename(filepath, new_path)
+    except FileExistsError:
+        os.remove(filepath)
+
+
+def get_md5(filepath):
+    # Open,close, read file and calculate MD5 on its contents
+    with open(filepath, 'rb') as file_to_check:
+        # read contents of the file
+        data = file_to_check.read()
+        # pipe contents of the file through
+        md5_returned = hashlib.md5(data).hexdigest()
+
+    # Finally compare original MD5 with freshly calculated
+    return md5_returned
+
+
+def webp_to_png_and_fix_gif(folder_path):
     for file in os.listdir(folder_path):
         if ".webp" in file:
             webp_path = os.path.join(folder_path, file)
             print(f"Found Webp! {webp_path}")
+            logging.info(f"Found Webp! {webp_path}")
             png_path = webp_path.replace('webp', 'png')
             cmd = f'{STATIC} "{webp_path}" -o "{png_path}"'
             result = subprocess.run(
                 cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)
             if result.returncode != 0:
                 print("Error occurred:", result.stderr.decode())
+                logging.error("Error occurred:", result.stderr.decode())
             else:
                 os.remove(webp_path)
+
+        if ".gif" in file:
+            gif_path = os.path.join(folder_path, file)
+            temp_path = os.path.join(folder_path, 'temp.gif')
+            print(f"Found Gif! {file}")
+            logging.info(f"Found Gif! {file}")
+            cmd = f'ffmpeg -i "{gif_path}" -y "{temp_path}"'
+            result = subprocess.run(
+                cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)
+            if result.returncode != 0:
+                print("Error occurred:", result.stderr.decode())
+                logging.error("Error occurred:", result.stderr.decode())
+            else:
+                os.replace(temp_path, gif_path)
                 
 
 def normal():
     for index, url in enumerate(URLS, start=1):
 
-        soup, content = get_soup_from_webpage(url, HEADER, timeout = 15)
+        soup, content = get_soup_from_webpage(url, HEADER, timeout=15)
 
         title = find_title(url, soup)
-        
+
         if title == 'Weixin Official Accounts Platform':
+            logging.error("疑似遇到服务器抽风，等待5秒钟后重试")
+            logging.error(url)
+            logging.error(title)
             print("疑似遇到服务器抽风，等待5秒钟后重试")
             time.sleep(5)
-            soup, content = get_soup_from_webpage(url, HEADER, timeout = 15)
+            soup, content = get_soup_from_webpage(url, HEADER, timeout=15)
             title = find_title(url, soup)
 
         gfw_title = [
             "The content has been deleted by the author.",
+            "This account has been deleted by the owner. Unable to view the content.",
             "该内容已被发布者删除",
             "此账号已被屏蔽, 内容无法查看",
+            "此内容因违规无法查看",
         ]
 
-        if title in gfw_title:
+        if title.strip() in gfw_title:  # 此内容因违规无法查看前后有空段
             print(f"  {index} 来晚了，内容被和谐了。（；´д｀）ゞ  ".center(78, "#"))
+            logging.error(f"{title}  {url}  {index} 来晚了，内容被和谐了。（；´д｀）ゞ  ".center(78, "#"))
             print(title)
             print(url)
             print()
@@ -266,11 +338,21 @@ def normal():
             break
 
         print(index, title, url)
+        logging.info(index)
+        logging.info(title)
+        logging.info(url)
 
-        downlist = extract_image_url(soup)
+        # downlist = extract_image_url(soup)
+        # if not downlist:
+        #     print("cdn")
+        #     downlist = extract_image_from_cdn_version_page(content)
+
+        downlist = extract_image_using_re_only(content)
         if not downlist:
-            print("cdn")
-            downlist = extract_image_from_cdn_version_page(content)
+            print("wtf, chatGPT?!")
+
+
+        logging.info(downlist)
 
         # skip emoji
         for link in downlist[:]:
@@ -293,6 +375,7 @@ def normal():
             os.mkdir(folder_path)
 
         print(f"共找到{len(downlist)}张图片")
+        logging.info(f"共找到{len(downlist)}张图片")
 
         threads = []
         for img_url in downlist:
@@ -302,8 +385,8 @@ def normal():
         for t in threads:
             t.join()
 
-        # Check and convert WebP to PNG
-        webp_to_png(folder_path)
+        # Check and convert WebP to PNG and also fix Gif
+        webp_to_png_and_fix_gif(folder_path)
         print()
 
 
